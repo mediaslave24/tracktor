547ca8eed79520719ef8470e3a20e6ef6c2d4b11
Michael <mmaccoffe@gmail.com>
2013-05-25 19:45:31 +0300

    Outputting errors, validating, states

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,6 +2,20 @@ class Issue < ActiveRecord::Base
   attr_accessible :assigned_to, :body, :customer_email, :customer_name, :state, :title
   default_scope order('created_at DESC')
 
+  validates_presence_of :body, :customer_email, :customer_name, :state, :title
+
+  STATES = %w{
+    waiting_for_staff
+    waiting_for_customer
+    onhold
+    cancelled
+    completed
+  }
+
+  before_validation do |record|
+    record.state ||= STATES.first
+  end
+
   def assigned_to
     assignee = Object.new
     def assignee.name
