c6e24924e6122a98694f6c2f5ece94e738f634e5
Michael <mmaccoffe@gmail.com>
2013-05-25 18:35:09 +0300

    Initial

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+# Web Framework
+gem 'rails', '3.2.13'
+
+# Database
+gem 'mysql2'
+
+
+group :assets do
+  gem 'haml'
+end
+
+# Helpers
+gem 'jquery-rails'
+gem 'devise'
+gem 'will_paginate'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracktor::Application.config.secret_token = 'a7e24df078ed7d91f0b562f030f1c7677402318ae9b950f32f5eef9977e446ea5049fc69caf4a65e427236721e82bc80ba8b383a7d950bfdf39402fd59b5aa30'
