e895981a82f8129136c433915f9304f1f69ad39a
Michael <mmaccoffe@gmail.com>
2013-05-25 21:11:07 +0300

    Quickfix

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,7 +2,7 @@ class Issue < ActiveRecord::Base
   attr_accessible :assigned_to, :body, :customer_email, :customer_name, :state, :title
   default_scope order('created_at DESC')
 
-  scope :unassigned, ->{ where(assigned_to: nil) }
+  scope :unassigned, ->{ where(assignee_id: nil) }
   scope :onhold,     ->{ where(state: "onhold") }
   scope :opened,     ->{ where(state: %w{ onhold waiting_for_staff waiting_for_customer }) }
   scope :closed,     ->{ where(state: %w{ cancelled completed }) }
